fec0f16926fe2b63041886e2652f76b07e1bcd32
Eric Minikel <ericminikel@gmail.com>
2016-03-08 17:11:51 -0500

    some more plots

--- src/compare_peaks.R
+++ src/compare_peaks.R
@@ -29,6 +29,12 @@ best_matches = candidate_matches[!duplicated(candidate_matches$cpeak),]
 
 best_matches$abs_delta_trunc = pmin(best_matches$abs_delta, .2)
 
+hist(best_matches$delta, breaks=10000, border=NA, col='#FF9912',  xaxs='i', yaxs='i', xlim=c(-.1,.1),
+     xlab='absolute difference in chemical shift (control vs. protein)',
+     ylab='number of peaks',
+     main='histogram of shift data')
+
+
 hist(best_matches$abs_delta_trunc, breaks=100, border=NA, col='#FF9912', xaxt='n', xaxs='i', yaxs='i',
      xlab='absolute difference in chemical shift (control vs. protein)',
      ylab='number of peaks',
@@ -43,3 +49,41 @@ best_matches$intens_delta = best_matches$cintens - best_matches$pintens
 plot(best_matches$abs_delta, abs(best_matches$intens_delta), pch=20, col='#FF9912', log='xy',
      xlab='Absolute change in chemical shift (log scale)', ylab='Absolute change in intensity (log scale)',
      main='Changes in peaks between control and protein conditions')
+
+best_matches$intens_ratio = best_matches$pintens / best_matches$cintens
+best_matches$delta_trunc = best_matches$delta
+best_matches$delta_trunc[best_matches$delta > .2] = .2
+best_matches$delta_trunc[best_matches$delta < -.2] = -.2
+plot(best_matches$delta_trunc, best_matches$intens_ratio, pch=20, col='#FF9912', log='y',
+     xlab='Change in chemical shift', ylab='Ratio of intensity',
+     main='Changes in peaks between control and protein conditions')
+abline(h=1,col='red')
+abline(v=0,col='red')
+
+# Ben Chittick's suggestion
+best_matches$shift_med_z = (best_matches$delta - median(best_matches$delta))/mad(best_matches$delta)
+sum(abs(best_matches$shift_med_z) > 5)
+
+best_matches$intens_med_z = (log(best_matches$intens_ratio) - median(log(best_matches$intens_ratio)))/mad(log(best_matches$intens_ratio))
+sum(abs(best_matches$intens_med_z) > 5)
+
+best_matches$hit = abs(best_matches$shift_med_z) > 5 | abs(best_matches$intens_med_z) > 5
+best_matches$col = '#FF9912'
+best_matches$col[best_matches$hit] = '#0000CD'
+
+
+plot(best_matches$delta_trunc, best_matches$intens_ratio, pch=20, col=best_matches$col, log='y',
+     xlab='Change in chemical shift', ylab='Ratio of intensity',
+     main='Changes in peaks between control and protein conditions')
+abline(h=1,col='red')
+abline(v=0,col='red')
+sum(best_matches$hit)
+
+# print list of hits
+best_matches$cpeak[best_matches$hit]
+
+table(paste(best_matches$row[best_matches$hit],best_matches$column[best_matches$hit],sep=''))
+
+
+
+
